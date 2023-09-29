66f77292dc8318bf3efd437d5142f39649f68561
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-29 20:21:19 +0600

    module_8_device_preview and Sizer

---
+++ lib/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:sizer/sizer.dart';
+
+/// Stack, Positioned, Align
+/// responsive Layout builder: responsive_builder
+/// Device preview : device_preview
+/// Size (height, width) : Sizer
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: ResponsiveBuilder(
+        builder: (context, sizeInformation) {
+          return Center(
+            child: Text(
+              sizeInformation.deviceScreenType.toString(),
+              style: TextStyle(
+                  fontSize: 15.sp
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
